0d792dc65b50762217d64039464325cc7fd1b73b
DavidM <medvedev03dv@gmail.com>
2025-11-17 11:53:52 +0500

    create

--- Новая папка/BIM/PS_BIM.extension/PS_BIM.tab/ModelChecker.panel/Копировать.pushbutton/config.py
+++
@@ -1,7 +0,0 @@
-"""Config for opening the manual"""
-# -*- coding: utf-8 -*-
-import os
-
-pdf_path = r"\\fs\public\Холдинг\ПоревитД\ТИМ\01_Библиотека\01_Рабочие задачи\BIM_Менеджмент\PS_BIM.extension\PS_BIM.tab\ModelChecker.panel\Копировать.pushbutton\00. Инструкция по выводу листов.docx"
-os.startfile(pdf_path)
-
